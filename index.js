97d21fae2a4a62db589a4e49687e2065f70da971
Harold Torres Marino <haroldtm55@gmail.com>
2021-06-04 19:09:12 +1000

    Tests passed improved code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
   }
   static titleize(string) {
     const array = string.split(' ')    
@@ -24,10 +24,8 @@ class Formatter {
         array[i] === 'by' ||
         array[i] === 'from')) {
           capitalizedAll.push(array[i])
-        } else capitalizedAll.push(this.capitalize(array[i]))
+      } else capitalizedAll.push(this.capitalize(array[i]))
     }
-    this.capitalize(capitalizedAll[0])
-    const titleized = capitalizedAll.reduce((init,acc)=>init+' '+acc)
-    return titleized
+    return capitalizedAll.join(' ')
   } 
 }
